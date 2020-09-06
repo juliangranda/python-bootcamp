3380ba31bf1b31a407446f31e7d1b0754ba9826c
juliangranda <juliangranda@hotmail.com>
2020-09-06 08:49:41 -0500

    logical operators

---
+++ seccion3/operadores_logicos.py
@@ -0,0 +1,10 @@
+# and , ambas condicionales igual a true
+print(1 < 2 and 2 < 3)
+print('h' == 'h' and 2 ==2)
+
+#or una de las condiciones es true
+print(1 == 1 or 2 == 2)
+print(100 == 1 or 2 == 2)
+
+#not devuleve el resultado booleano contrario , si es false devuleve tru y viceversa
+print(not( 400 > 500))
